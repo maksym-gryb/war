ae77a64e81265d8ffb6068c335a955cf058cf92c
maksym-gryb <maksym.gryb@gmail.com>
2017-09-29 23:03:45 -0400

    Default ticks per update is now 1

--- inc/scene.h
+++ inc/scene.h
@@ -5,7 +5,7 @@
 class Scene{
 	public:
 		Scene(sf::RenderWindow* p_window);
-		virtual bool update(int p_ticks) = 0;
+		virtual bool update(int p_ticks = 1) = 0;
 
 	private:
 		sf::RenderWindow* _window;
