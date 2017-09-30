5d91efa8596794da2a7ca040476e91a9023109db
maksym-gryb <maksym.gryb@gmail.com>
2017-09-29 20:07:52 -0400

    Add base class Scene

---
+++ inc/scene.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Scene{
+	public:
+		Scene(sf::RenderWindow* p_window);
+		virtual bool update(int p_ticks) = 0;
+
+	private:
+		sf::RenderWindow* _window;
+};
---
+++ src/scene.cpp
@@ -0,0 +1,6 @@
+#include "scene.h"
+
+Scene::Scene(sf::RenderWindow* p_window)
+{
+	_window = p_window;
+}
