afce92cdbcef9c360e465f62d6cea6410b50eb46
maksym-gryb <maksym.gryb@gmail.com>
2017-10-03 16:47:43 -0400

    Move application logic into own class

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,10 @@
 #include <SFML/Graphics.hpp>
 
-#include "testscene.h"
+#include "app.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "WAR!");
-    Scene* current_scene = new TestScene(&window);
+	App* app = new App();
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-	current_scene->update();
-    }
-
-    return 0;
+	return app->run();
 }
