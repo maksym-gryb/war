e4151867e805613dc235e14bc6c1bd6c785e1df0
maksym-gryb <maksym.gryb@gmail.com>
2017-10-03 20:21:26 -0400

    Arbitrate view movement frequency and speed

--- inc/app.h
+++ inc/app.h
@@ -4,6 +4,9 @@
 
 #include "testscene.h"
 
+const float INPUT_RATE = 1000/60;
+const float SCREEN_MOVE_SPEED = 4;
+
 class App{
 	public:
 		App();// add error handling later
@@ -14,4 +17,6 @@ class App{
 	private:
 		sf::RenderWindow* _window;
 		Scene* _current_scene;
+
+		sf::Clock _clock_input;
 };
--- src/app.cpp
+++ src/app.cpp
@@ -2,8 +2,7 @@
 
 App::App()
 {
-	_window = new sf::RenderWindow(sf::VideoMode(200, 200), "WAR!");
-	_current_scene = new TestScene(_window);
+	_clock_input.restart();
 }
 
 App::~App()
@@ -11,52 +10,60 @@ App::~App()
 
 int App::run()
 {
-    while (_window->isOpen())
-    {
-        sf::Event event;
-        while (_window->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                _window->close();
-        }
+	sf::RenderWindow window(sf::VideoMode(200, 200), "WAR!");
+	Scene* current_scene = new TestScene(&window);
 
+	while (window.isOpen())
+	{
+        	sf::Event event;
+        	while (window.pollEvent(event))
+        	{
+            		if (event.type == sf::Event::Closed)
+                		window.close();
+        	}
+
+		if(_clock_input.getElapsedTime().asMilliseconds()
+			> INPUT_RATE)
+		{
+			_clock_input.restart();
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		sf::View view = _window->getView();
+		sf::View view = window.getView();
 
-		view.move(0, -0.1);
+		view.move(0, -SCREEN_MOVE_SPEED);
 
-		_window->setView(view);
+		window.setView(view);
 	}
 
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		sf::View view = _window->getView();
+		sf::View view = window.getView();
 
-		view.move(0.1, 0);
+		view.move(SCREEN_MOVE_SPEED, 0);
 
-		_window->setView(view);
+		window.setView(view);
 	}
 
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
-		sf::View view = _window->getView();
+		sf::View view = window.getView();
 
-		view.move(0, 0.1);
+		view.move(0, SCREEN_MOVE_SPEED);
 
-		_window->setView(view);
+		window.setView(view);
 	}
 
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		sf::View view = _window->getView();
+		sf::View view = window.getView();
 
-		view.move(-0.1, 0);
+		view.move(-SCREEN_MOVE_SPEED, 0);
 
-		_window->setView(view);
+		window.setView(view);
 	}
+		}
 
-	_current_scene->update();
+	current_scene->update();
     }
 
     return 0;
